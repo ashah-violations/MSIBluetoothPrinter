21f6debd0757369bb8e09dc0947ec5c47d0148d0
avinash shah <shahavinash@hotmail.com>
2017-01-09 11:35:39 -0600

    changes to the method

--- www/MSIBluetoothPrinter.js
+++ www/MSIBluetoothPrinter.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.print = function(mac, str, successCallback, errorCallback) {
+exports.print = function(str, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'MSIBluetoothPrinter', 'print', [str]);
 };
 

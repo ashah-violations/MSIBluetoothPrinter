1c37a466cee77ca22a0456a5bcd51ae2fe5c29e4
avinash shah <shahavinash@hotmail.com>
2017-01-09 12:57:58 -0600

    changes for ios

--- www/MSIBluetoothPrinter.js
+++ www/MSIBluetoothPrinter.js
@@ -4,6 +4,6 @@ exports.print = function(str, successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'MSIBluetoothPrinter', 'print', [str]);
 };
 
-exports.find = function(successCallback, errorCallback) {
+exports.findPrinters = function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'MSIBluetoothPrinter', 'find', []);
 };

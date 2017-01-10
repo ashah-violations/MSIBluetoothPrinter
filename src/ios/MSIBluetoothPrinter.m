c81db15c671bc809f25dc2c7c05e41771dc55085
Avinash <ashah-violations@users.noreply.github.com>
2017-01-10 15:08:59 -0600

    Update MSIBluetoothPrinter.m

--- src/ios/MSIBluetoothPrinter.m
+++ src/ios/MSIBluetoothPrinter.m
@@ -1,96 +1,149 @@
+package msi.cordova.bluetooth.zbtprinter;
 
-#import "MSIBluetoothPrinter.h"
-#import <ExternalAccessory/ExternalAccessory.h>
-#import "MfiBtPrinterConnection.h"
-#import "ZebraPrinterConnection.h"
-#import "MFiBtPrinterConnection.h"
-#import "ZebraPrinterFactory.h"
-#import "SGD.h"
+import java.io.IOException;
+//import android.os.Looper;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.util.Log;
+import com.zebra.android.discovery.*;
+import com.zebra.sdk.comm.*;
+import com.zebra.sdk.printer.*;
 
-@implementation MSIBluetoothPrinter
-- (void) sendData:(CDVInvokedUrlCommand *)command{
+public class MSIBluetoothPrinter extends CordovaPlugin {
+    private static DiscoveryHandler btDiscoveryHandler = null;
+    private static final String LOG_TAG = "MSIBluetoothPrinter";
+    String mac = "";
 
-    NSString *labelData = [command.arguments objectAtIndex:0];
-    //NSString *serielCode = [command.arguments objectAtIndex:1];
-
-    // Check command.arguments here.
-    //Dispatch this task to the default queue
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^ {
-
-        // Instantiate connection to Zebra Bluetooth accessory
-        //Find the Zebra Bluetooth Accessory
-        NSString *serialNumber = @"";
-        EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-        NSArray * connectedAccessories = [sam connectedAccessories];
-        for (EAAccessory *accessory in connectedAccessories) {
-            if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-                serialNumber = [serialNumber stringByAppendingString:accessory.serialNumber];
-                break;
-            }
-        }
-        id<ZebraPrinterConnection, NSObject> thePrinterConn = [[MfiBtPrinterConnection alloc] initWithSerialNumber:serialNumber];
-
-        // Open the connection - physical connection is established here.
-        BOOL success = [thePrinterConn open];
-
-        if(success)
-        {
-            NSError *error = nil;
-            //[SGD SET:@"ezpl.media_type" withValue:@"gap/notch" andWithPrinterConnection:thePrinterConn error:&error];
-
-            // This example prints "This is a ZPL test." near the top of the label.
-            //NSString *zplData = @"^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ";
-
-
-            // Send the data to printer as a byte array.
-            NSData *data = [NSData dataWithBytes:[labelData UTF8String] length:[labelData length]];
-
-            success = success && [thePrinterConn write:data error:&error];
-
-            [NSThread sleepForTimeInterval:2.0f];
-
-            if (success == YES && error == nil) {
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            }else{
-                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            }
-
-            // Close the connection to release resources.
-            [thePrinterConn close];
-        }else{
-            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Printer Connection Failed"];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        }
-
-     });
-}
-//Returns all the connected printers.
-- (void) findPrinter:(CDVInvokedUrlCommand *)command
-{
-    NSString *serialNumber = @"";
-    //Find the Zebra Bluetooth Accessory
-    EAAccessoryManager *sam = [EAAccessoryManager sharedAccessoryManager];
-    NSArray * connectedAccessories = [sam connectedAccessories];
-    for (EAAccessory *accessory in connectedAccessories) {
-        if([accessory.protocolStrings indexOfObject:@"com.zebra.rawport"] != NSNotFound){
-            serialNumber = [serialNumber stringByAppendingString:accessory.serialNumber];
-            serialNumber = [serialNumber stringByAppendingString:@","];
-            //serialNumber = accessory.serialNumber;
-        }
+    public MSIBluetoothPrinter() {
     }
 
-    if([serialNumber isEqual:@""])
-    {
-        //No printer found empty
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No Serial Number Found."];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }else{
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:serialNumber];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        if (action.equals("sendData")) {
+            try {
+                String msg = args.getString(0);
+                String mac = args.getString(1);
+                sendData(callbackContext, msg,mac);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, e.getMessage());
+                e.printStackTrace();
+            }
+            return true;
+        }
+        if (action.equals("findPrinter")) {
+            try {
+                String mac = args.getString(0);
+                findMac(callbackContext,mac);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, e.getMessage());
+                e.printStackTrace();
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    public void findPrinter(final CallbackContext callbackContext) {
+        new Thread(new Runnable() {
+            public void run() {
+                btDiscoveryHandler=new DiscoveryHandler() {
+                    public void foundPrinter(final DiscoveredPrinter printer) {
+                        String macAddress = printer.address;
+                        //I found a printer! I can use the properties of a Discovered printer (address) to make a Bluetooth Connection
+                        callbackContext.success(macAddress);
+                    }
+                    public void discoveryFinished() {
+                        //Discovery is done
+                        callbackContext.success("");
+                    }
+                    public void discoveryError(String message) {
+                        //Error during discovery
+                        callbackContext.error(message);
+                    }
+                };
+                try {
+                    BluetoothDiscoverer.findPrinters(cordova.getActivity().getApplicationContext(),btDiscoveryHandler);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    public void findMac(final CallbackContext callbackContext,final String macAddress) {
+        try {
+            Connection thePrinterConn = new BluetoothConnectionInsecure(macAddress);
+            if (isPrinterReady(thePrinterConn)) {
+                callbackContext.success("Done");
+            }
+            else{
+                callbackContext.error("Printer is not ready");
+            }
+            
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+        }
+    }
+    /*
+     * This will send data to be printed by the bluetooth printer
+     */
+    void sendData(final CallbackContext callbackContext, final String msg, final String mac) throws IOException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Instantiate insecure connection for given Bluetooth MAC Address.
+                    Connection thePrinterConn = new BluetoothConnectionInsecure(mac);
+
+                    // Verify the printer is ready to print
+                    if (isPrinterReady(thePrinterConn)) {
+
+                        // Open the connection - physical connection is established here.
+                        thePrinterConn.open();
+
+                        // Send the data to printer as a byte array.
+						// thePrinterConn.write("^XA^FO0,20^FD^FS^XZ".getBytes());
+                        thePrinterConn.write(msg.getBytes());
+
+
+                        // Make sure the data got to the printer before closing the connection
+                        Thread.sleep(500);
+
+                        // Close the insecure connection to release resources.
+                        thePrinterConn.close();
+                        callbackContext.success("Done");
+                    } else {
+						callbackContext.error("Printer is not ready");
+					}
+                } catch (Exception e) {
+                    // Handle communications error here.
+                    callbackContext.error(e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    private Boolean isPrinterReady(Connection connection) throws ConnectionException, ZebraPrinterLanguageUnknownException {
+        Boolean isOK = false;
+        connection.open();
+        // Creates a ZebraPrinter object to use Zebra specific functionality like getCurrentStatus()
+        ZebraPrinter printer = ZebraPrinterFactory.getInstance(connection);
+        PrinterStatus printerStatus = printer.getCurrentStatus();
+        connection.close();
+        if (printerStatus.isReadyToPrint) {
+            isOK = true;
+        } else if (printerStatus.isPaused) {
+            throw new ConnectionException("Cannot print because the printer is paused");
+        } else if (printerStatus.isHeadOpen) {
+            throw new ConnectionException("Cannot print because the printer media door is open");
+        } else if (printerStatus.isPaperOut) {
+            throw new ConnectionException("Cannot print because the paper is out");
+        } else {
+            throw new ConnectionException("Cannot print");
+        }
+        return isOK;
     }
 }
 
-@end
